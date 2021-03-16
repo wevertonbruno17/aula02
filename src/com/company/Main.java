10b6d7704ae52cb6922d28d55eab827bc0c3a22c
wevertonbruno17 <w.vilvock17@gmail.com>
2021-03-15 21:54:41 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,99 @@
+package com.company;
+
+import com.sun.xml.internal.stream.events.StartDocumentEvent;
+
+public class Main {
+
+    //INICIALIZADOR
+    public static void main(String[] args) {
+        /*
+	 String nome="weverton";
+	 System.out.println(nome);
+
+
+	 String nome2="wevertoBruno";
+	 System.out.println("nome2: " + nome2);
+
+	 String nome3="WevertonVilvock";
+	 String frase= "Meu Nome é "+ nome3;
+	 System.out.println(frase);
+
+
+        String nome = "vitor";
+        String nome2 = "luana";
+        String nome3 = "Jessika";
+        String nome4 = "bruno";
+        String frase = nome + " é casado com " + nome2 + " é tem dois Filhos " + nome3 + " e " + nome4;
+        System.out.println(frase);
+
+
+
+        int x = 110;
+        int y = 2;
+
+        int soma = x + y;
+        int multiplicacao = x * y;
+        int subtracao = x - y;
+        int divisao = x / y;
+
+        System.out.println(" O resuldado da soma é " + soma);
+        System.out.println(" O resultado da multiplicacao é " + multiplicacao);
+        System.out.println(" O resultado da subtracao é " + subtracao);
+        System.out.println(" O resultado da divisao é " + divisao);
+
+
+        int baseMaior = 20;
+        int baseMenor = 10;
+        int altura = 30;
+        int divisao = 3;
+
+        int resultado = (baseMaior + baseMenor)* altura / divisao;
+
+        System.out.println(" A área do trapezio é " + resultado);
+
+        if (resultado == 450 ){
+            System.out.println(" O RESULTADO É VALIDO");
+            } else {
+            System.out.println("O resultado é invalido");
+        }
+        }
+
+        int idade = 70;
+        boolean brasileiro = true;
+        boolean turnoDaTarde = true;
+
+
+        if (idade >= 18 && idade <= 60) {
+            if (brasileiro) {
+                if (turnoDaTarde) {
+                    System.out.println("Usuario pode se Cadastrar");
+                } else {
+                    System.out.println("Usuario não pode se cadastrar");
+                }
+            } else {
+                System.out.println(" O Usuario não é Elegivel");
+            }
+        } else {
+            System.out.println(" O Usuario não é Elegivel");
+        }
+
+        */
+
+        /*
+
+        int idade = 23;
+        verificarIdade(idade);
+    }
+
+    private static void verificarIdade(int idade) {
+        if (idade >= 18){
+            System.out.println(" é maior de idade");
+        } else {
+            System.out.println("Não é maior de idade");
+        }
+
+    }
+}
+*/
+
+
